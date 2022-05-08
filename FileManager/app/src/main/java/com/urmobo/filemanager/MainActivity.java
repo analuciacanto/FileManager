617e168a4484394df671e53bdb03fb3b5c9558e2
Ana Canto <anacanto@Ana.anacanto.com.br>
2022-05-08 16:49:37 -0300

    Adding List View for files.

--- FileManager/app/src/main/java/com/urmobo/filemanager/MainActivity.java
+++ FileManager/app/src/main/java/com/urmobo/filemanager/MainActivity.java
@@ -3,12 +3,91 @@ package com.urmobo.filemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout1);
+
+        final Button b1 = findViewById(R.id.b1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1.setText("pressed");
+            }
+        });
+
+        final ListView listView = findViewById(R.id.listView);
+        final TextAdapter textAdapter1 = new TextAdapter();
+        listView.setAdapter(textAdapter1);
+        List<String> example = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++){
+            example.add(String.valueOf(i));
+        }
+        System.out.println(example);
+        textAdapter1.setData(example);
+
+    }
+
+    class TextAdapter extends BaseAdapter{
+
+        private List<String> data = new ArrayList<>();
+        public void setData(List<String> data){
+            if (data != null){
+                this.data.clear();
+                if (data.size() > 0){
+                    this.data.addAll(data);
+                }
+                notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+                convertView.setTag(new ViewHolder((TextView) convertView.findViewById(R.id.textItem)));
+            }
+            ViewHolder holder  = (ViewHolder) convertView.getTag();
+            final String item = getItem(position);
+            holder.info.setText(item);
+            return convertView;
+        }
+
+        class ViewHolder{
+            TextView info;
+            ViewHolder(TextView info){
+                this.info = info;
+            }
+        }
     }
 }
